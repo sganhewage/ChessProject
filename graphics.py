4d616528e0d27684095e786d1d4f9943fc221613
sganhewage <sganhewage@guilford.edu>
2022-08-25 19:12:28 -0400

    initial screen

--- graphics.py
+++ graphics.py
@@ -1,2 +1,12 @@
-print("Hello world")
-print("f")
+#import pygame
+import pygame
+
+#initialize pygame
+pygame.init()
+
+#define constants for the screen width and height
+screen_width = 800
+screen_height = 800
+
+#create the screen object; size determined by screen_width and screen_height
+screen = pygame.display.set_mode((screen_width, screen_height))
