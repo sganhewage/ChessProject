2c38736d86940d7ab057d97bccdac9690b5d5905
alexdyk <alexdykang@gmail.com>
2022-08-25 21:54:18 -0400

    YESSIR

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-# imports
+#imports
 import pygame
 from PIL import Image
 
@@ -15,3 +15,19 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 #the actual board
 img = Image.open("Chess Graphics/chess graphics/chess board/chessboard.jpg")
 img.show()
+
+#putting the image on the board
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Chess Graphics/chess graphics/chess board/chessboard.jpg')
+chessboard = pygame.image.load("Chess Graphics/chess graphics/chess board/chessboard.jpg").convert()
+x = 100
+y = 30
+screen.blit(chessboard, (x, y))
+pygame.display.flip()  #shows the chessboard
+
+running = True
+while (running): #press end game then loop stops
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
